fcbf3da301d2e9fe85ec8993f6421808ff0bfd4c
klozz <klozz707@gmail.com>
2017-10-10 02:32:42 -0400

    fix nfc build
    Signed-off-by: klozz <klozz707@gmail.com>

--- src/com/android/nfc/NfcService.java
+++ src/com/android/nfc/NfcService.java
@@ -797,6 +797,9 @@ public class NfcService implements DeviceHostListener {
     }
 
     final class NfcAdapterService extends INfcAdapter.Stub {
+        public IBinder getNfcAdapterVendorInterface(String vendor) {
+            return null;
+        }
         @Override
         public boolean enable() throws RemoteException {
             NfcPermissions.enforceAdminPermissions(mContext);
